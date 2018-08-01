9dbfff68ce29c644987ad69cea11eed5e167f6c6
nimbus117 <mail@nimbus117.co.uk>
2018-08-01 18:03:28 +0100

    addd some spaces

--- exercise_a_stops.rb
+++ exercise_a_stops.rb
@@ -1,9 +1,11 @@
 stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
 
 # 1. Add `"Edinburgh Waverley"` to the end of the array
+
 stops.push("Edinburgh Waverly")
 
 # 2. Add `"Glasgow Queen St"` to the start of the array
+
 stops.unshift("Glasgow Queen St")
 
 # 3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and `"Linlithgow"`)
